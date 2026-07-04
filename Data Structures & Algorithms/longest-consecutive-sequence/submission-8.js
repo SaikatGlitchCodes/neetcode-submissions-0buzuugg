6856eb8e82b69e23f0d959f2e3dfad133f62e234
SAIKAT SAMANTA <saikatsamanta052@gmail.com>
2026-07-04 08:03:19 +0530

    Add: longest-consecutive-sequence - submission-8

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-8.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    longestConsecutive(nums) {
+        const set = new Set(nums);
+        let result = 0;
+
+        for(let num of set){
+            if(!set.has(num-1)){
+                console.log(num)
+                let iterator = 0;
+                let curr = num
+                while(set.has(curr)) {
+                    iterator++
+                    curr++
+                }
+                result = Math.max(result, iterator)
+            }
+        }
+        return result
+    }
+}
