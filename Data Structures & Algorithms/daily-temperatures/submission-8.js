cbdd6c4f32317c4bb9b56e2b528bc16c13d479a9
SAIKAT SAMANTA <saikatsamanta052@gmail.com>
2026-07-05 14:12:27 +0530

    Add: daily-temperatures - submission-8

---
+++ Data Structures & Algorithms/daily-temperatures/submission-8.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {number[]} temperatures
+     * @return {number[]}
+     */
+    dailyTemperatures(temperatures) {
+        const stack = [0];
+        const result = new Array(temperatures.length).fill(0);
+        for(let i=1; i<temperatures.length; i++){
+            while(temperatures[i] > temperatures[stack[stack.length-1]] && stack.length){
+                result[stack[stack.length-1]] = i-stack[stack.length-1]
+                stack.pop()
+            }
+            console.log(stack, result)
+            stack.push(i)
+        }
+        return result
+    }   
+}
