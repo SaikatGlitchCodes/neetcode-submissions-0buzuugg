954cc2c1782d0fd1c6f7e92e123c69bd12eb1e7d
SAIKAT SAMANTA <saikatsamanta052@gmail.com>
2026-07-08 17:05:00 +0530

    Add: car-fleet - submission-0

---
+++ Data Structures & Algorithms/car-fleet/submission-0.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number} target
+     * @param {number[]} position
+     * @param {number[]} speed
+     * @return {number}
+     */
+    carFleet(target, position, speed) {
+        const pair = position.map((v,i)=> [v, speed[i]]);
+        const stack = [];
+        const sortedPair = pair.sort((a, b) => (b[0]-a[0]));
+        
+        for(let t of sortedPair){
+            console.log((stack[stack.length-1] || 0), ((target-t[0])/t[1] ))
+            if((stack[stack.length-1] || 0) < ((target-t[0])/t[1] )){
+                stack.push((target-t[0])/t[1] )
+            }
+        }
+        return stack.length
+    }
+}
