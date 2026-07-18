d4ad8dca95a2fb86523803a0370d55fefa585bc9
SAIKAT SAMANTA <saikatsamanta052@gmail.com>
2026-07-18 12:39:33 +0530

    Add: products-of-array-discluding-self - submission-12

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-12.js
@@ -0,0 +1,16 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    productExceptSelf(nums) {
+        let arr_left=[]
+        let arr_right=[]
+        for(let i=0; i<nums.length; i++){
+            arr_left[i] =( arr_left[i-1] ?? 1) * (nums[i-1] ?? 1);
+            arr_right[nums.length - 1 - i] =
+    (arr_right[nums.length - i] ?? 1) * (nums[nums.length - i] ?? 1);
+        }
+        return nums.map((v,i)=> arr_left[i]*arr_right[i])
+    }
+}
