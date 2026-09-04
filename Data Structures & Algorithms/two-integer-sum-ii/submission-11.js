2be365db0b8a8cc5fdd1355db07460c3cb175d58
SAIKAT SAMANTA <saikatsamanta052@gmail.com>
2026-09-04 17:09:54 +0530

    Add: two-integer-sum-ii - submission-11

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-11.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {number[]} numbers
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(numbers, target) {
+        let left = 0;
+        let right = numbers.length-1;
+        while(left < right){
+            let total = numbers[left]+ numbers[right];
+            if(target == total){
+                return [left+1, right+1]
+            }else if (target < total) right--
+            else left++
+        }
+        return []
+    }
+}
