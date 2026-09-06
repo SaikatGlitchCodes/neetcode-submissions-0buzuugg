76f13bec1e6d7219a523d4dcb84981852b5a0f43
SAIKAT SAMANTA <saikatsamanta052@gmail.com>
2026-09-06 20:30:50 +0530

    Add: next-permutation - submission-1

---
+++ Data Structures & Algorithms/next-permutation/submission-1.js
@@ -0,0 +1,38 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {void} Do not return anything, modify nums in-place instead.
+     */
+    
+    nextPermutation(nums) {
+        let i= nums.length-2;
+        while(i>=0 && nums[i+1] <= nums[i]){
+            i--
+        }
+        if(i>=0){
+            let j = nums.length-1;
+            while(nums[j] <= nums[i]){
+                j--;
+            }
+            swap(nums, i, j)
+        }
+        reverse(nums, i+1)
+        function swap(nums, i, j){
+            let temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+        }
+
+        function reverse(nums, i){
+            let j = nums.length-1;
+            while(i<j){
+                swap(nums, i, j);
+                i++;
+                j--;
+            }
+
+        }
+
+
+    }
+}
