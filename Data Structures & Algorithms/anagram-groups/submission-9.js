e7b91489e1f1226d76437293fe01848ab4314cb7
SAIKAT SAMANTA <saikatsamanta052@gmail.com>
2026-09-07 18:34:04 +0530

    Add: anagram-groups - submission-9

---
+++ Data Structures & Algorithms/anagram-groups/submission-9.js
@@ -0,0 +1,23 @@
+class Solution {
+    groupAnagrams(strs) {
+        const map = new Map();
+
+        for (const str of strs) {
+            const count = new Array(26).fill(0);
+
+            for (let i = 0; i < str.length; i++) {
+                count[str.charCodeAt(i) - 97]++;
+            }
+
+            const key = count.join('^');
+
+            if (!map.has(key)) {
+                map.set(key, []);
+            }
+
+            map.get(key).push(str);
+        }
+
+        return Array.from(map.values());
+    }
+}
