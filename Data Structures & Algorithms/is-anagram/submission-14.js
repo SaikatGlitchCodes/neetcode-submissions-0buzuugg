b6e01bde3d337c732e379c000aae53a22b71c47a
SAIKAT SAMANTA <saikatsamanta052@gmail.com>
2026-09-07 17:03:22 +0530

    Add: is-anagram - submission-14

---
+++ Data Structures & Algorithms/is-anagram/submission-14.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+        if(s.length !== t.length) return false
+        const arr = new Array(26).fill(0);
+        for(let i=0; i<s.length; i++){
+            arr[s[i].charCodeAt('a')-97]++
+            arr[t[i].charCodeAt('a')-97]--
+        }
+        for(let char of arr){
+            if(char != 0) return false
+        }
+        return true
+    }
+}
