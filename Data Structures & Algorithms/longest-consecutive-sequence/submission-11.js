70d795b3c4f1a86808639175297a4f0c9fe2774c
SAIKAT SAMANTA <saikatsamanta052@gmail.com>
2026-09-07 18:44:54 +0530

    Add: longest-consecutive-sequence - submission-11

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-11.js
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    longestConsecutive(nums) {
+        const set = new Set(nums);
+        let max = 0;
+        for(let num of nums){
+            let curr = 0;
+            let curnum = num
+            if(set.has(num-1)) continue
+            while(set.has(curnum++)) curr++
+            max = Math.max(curr, max)
+        }
+        return max
+    }
+}
