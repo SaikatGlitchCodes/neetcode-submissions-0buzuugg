d843d3f3fc514718aa0e217e92e68f7cdf00f5ea
SAIKAT SAMANTA <saikatsamanta052@gmail.com>
2026-09-07 20:01:09 +0530

    Add: majority-element-ii - submission-0

---
+++ Data Structures & Algorithms/majority-element-ii/submission-0.js
@@ -0,0 +1,17 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    majorityElement(nums) {
+        let k = Math.floor(nums.length/3);
+        const result = []
+        const map = new Map();
+        const arr = Array.from({length: nums.length+1}, ()=>[]);
+        for(let num of nums) map.set(num, (map.get(num)||0)+1);
+        for(let [key, val] of map){
+            if(val>k) result.push(key)
+        }
+        return result
+    }
+}
