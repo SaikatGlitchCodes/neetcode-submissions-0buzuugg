066a0604365c54c51dce2ddf21d821a1c2b0f3bd
SAIKAT SAMANTA <saikatsamanta052@gmail.com>
2026-09-13 13:52:34 +0530

    Add: palindromic-substrings - submission-3

---
+++ Data Structures & Algorithms/palindromic-substrings/submission-3.js
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {number}
+     */
+    countSubstrings(s) {
+        let count =0;
+        expand(true);
+        expand(false)
+        function expand(e){
+            for (let i = 0; i < s.length; i++) {
+                let l = i;
+                let r = e?i+1:i;
+
+                while (
+                    l >= 0 &&
+                    r < s.length &&
+                    s[l] === s[r]
+                ){
+                    count++
+                    l--;
+                    r++;
+                }
+            }
+        }
+        
+        return count
+    }
+}
+                                                                                                                                                
