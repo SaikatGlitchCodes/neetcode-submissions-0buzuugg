90c6b6ebed38bfa1dadf6593f1723c4af9709b29
SAIKAT SAMANTA <saikatsamanta052@gmail.com>
2026-09-18 08:14:35 +0530

    Add: eating-bananas - submission-1

---
+++ Data Structures & Algorithms/eating-bananas/submission-1.js
@@ -0,0 +1,28 @@
+class Solution {
+    /**
+     * @param {number[]} piles
+     * @param {number} h
+     * @return {number}
+     */
+    minEatingSpeed(piles, h) {
+        let l=1;
+        let r= Math.max(...piles);
+        let res =r;
+
+        while(l<=r){
+            const mid = Math.floor(l+(r-l)/2);
+            let totalTime = 0
+
+            for(const pile of piles){
+                totalTime += Math.ceil(pile/mid);
+            }
+            if(totalTime <= h){
+                res = mid;
+                r=mid-1
+            }else{
+                l=mid+1
+            }
+        }
+        return res
+    }
+}
